3ab5a77c3a75ee42c5d966557966ad720755bd5f
SpeakWithPlants <speakwithplants@gmail.com>
2023-04-04 01:58:18 -0400

    Added bonus damage while using the dark sword against an enemy vexed by abigail in nightmare form

--- src/scripts/features/updates_wendy.lua
+++ src/scripts/features/updates_wendy.lua
@@ -3,23 +3,28 @@ local elixirs = require "libraries/custom_elixirs_params"
 AddPrefabPostInit("wendy", function(wendy)
     if wendy.components.combat ~= nil then
         local old_customdamagemultfn = wendy.components.combat.customdamagemultfn
-        wendy.components.combat.customdamagemultfn = function(self, target)
+        wendy.components.combat.customdamagemultfn = function(self, target, weapon)
             local abigail = self.components.ghostlybond ~= nil and self.components.ghostlybond.ghost
-            local active_elixir = abigail:GetDebuff("elixir_buff")
+            local active_buff = abigail:GetDebuff("elixir_buff")
             local multiplier = 1
-            if active_elixir ~= nil and target:HasDebuff("abigail_vex_debuff") then
-                if active_elixir.prefab == "newelixir_healthdamage_buff" then
+            if active_buff ~= nil and target:HasDebuff("abigail_vex_debuff") then
+                if active_buff.prefab == "newelixir_healthdamage_buff" then
                     multiplier = elixirs.newelixir_healthdamage.calcmultiplier_wendy_vex(self, abigail)
-                elseif active_elixir.prefab == "newelixir_insanitydamage_buff" then
+                end
+                if active_buff.prefab == "newelixir_insanitydamage_buff" then
                     multiplier = elixirs.newelixir_insanitydamage.calcmultiplier_wendy_vex(self, abigail)
-                elseif active_elixir.prefab == "newelixir_shadowfighter_buff" then
+                end
+                if active_buff.prefab == "newelixir_shadowfighter_buff" then
                     if target:HasTag("shadowcreature") then
                         multiplier = TUNING.NEW_ELIXIRS.SHADOWFIGHTER.WENDY_VEX.DAMAGE_MULT
                     end
                 end
+                if active_buff.potion_tunings.nightmare and weapon ~= nil and weapon.prefab == "nightsword" then
+                    multiplier = multiplier * TUNING.NEW_ELIXIRS.ALL_NIGHTMARE_ELIXIRS.DARK_SWORD_VEX_MULT
+                end
             end
-            if old_customdamagemultfn ~= nil then
-                multiplier = multiplier * old_customdamagemultfn(self, target)
+            if multiplier == 1 and old_customdamagemultfn ~= nil then
+                return old_customdamagemultfn(self, target)
             end
             return multiplier
         end
