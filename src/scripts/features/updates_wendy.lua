3bb266a0f846c6ac410c174cab772e231d0af99b
SpeakWithPlants <speakwithplants@gmail.com>
2023-04-03 23:37:23 -0400

    Updated wendy's vex multiplier

--- src/scripts/features/updates_wendy.lua
+++ src/scripts/features/updates_wendy.lua
@@ -6,20 +6,22 @@ AddPrefabPostInit("wendy", function(wendy)
         wendy.components.combat.customdamagemultfn = function(self, target)
             local abigail = self.components.ghostlybond ~= nil and self.components.ghostlybond.ghost
             local active_elixir = abigail:GetDebuff("elixir_buff")
+            local multiplier = 1
             if active_elixir ~= nil and target:HasDebuff("abigail_vex_debuff") then
                 if active_elixir.prefab == "newelixir_healthdamage_buff" then
-                    return elixirs.newelixir_healthdamage.calcmultiplier_wendy_vex(self, abigail)
+                    multiplier = elixirs.newelixir_healthdamage.calcmultiplier_wendy_vex(self, abigail)
                 elseif active_elixir.prefab == "newelixir_insanitydamage_buff" then
-                    return elixirs.newelixir_insanitydamage.calcmultiplier_wendy_vex(self, abigail)
+                    multiplier = elixirs.newelixir_insanitydamage.calcmultiplier_wendy_vex(self, abigail)
                 elseif active_elixir.prefab == "newelixir_shadowfighter_buff" then
                     if target:HasTag("shadowcreature") then
-                        return TUNING.NEW_ELIXIRS.SHADOWFIGHTER.WENDY_VEX.DAMAGE_MULT
+                        multiplier = TUNING.NEW_ELIXIRS.SHADOWFIGHTER.WENDY_VEX.DAMAGE_MULT
                     end
                 end
             end
             if old_customdamagemultfn ~= nil then
-                return old_customdamagemultfn(self, target)
+                multiplier = multiplier * old_customdamagemultfn(self, target)
             end
+            return multiplier
         end
     end
 end)
