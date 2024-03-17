a4115c6ff942ed08667a557e0889cab590280c18
SpeakWithPlants <speakwithplants@gmail.com>
2024-03-17 16:52:12 -0400

    Updated version

--- src/modinfo.lua
+++ src/modinfo.lua
@@ -1,7 +1,7 @@
 name = "Ghostly Elixirs+"
 description = "Improves the potential of Wendy's Ectoherbology with new elixirs, grave mechanics, and offerings!"
 author = "SpeakWithPlants"
-version = "1.1.6"
+version = "1.1.7"
 
 forumthread = ""
 
